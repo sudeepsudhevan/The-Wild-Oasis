cf50150f112e8a88625a2e4d7ce0309a198e5358
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-12 08:29:38 +0530

    first commit

---
+++ src/features/bookings/useBookings.js
@@ -0,0 +1,50 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+
+export function useBookings() {
+  const queryClient = useQueryClient();
+  const [searchParams] = useSearchParams();
+
+  // 1) Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  // 2) Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
+  // 3) Pagination
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+  // 4) Query
+  const {
+    isLoading,
+    data: { data: bookings, count } = {},
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
+  });
+
+  // 5) Pre-fetching
+  const pageCount = Math.ceil(count / 10);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  return { isLoading, bookings, error, count };
+}
---
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -0,0 +1,72 @@
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
+import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
+
+function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
+
+  const { isUpdating, updateSetting } = useUpdateSetting();
+
+  if (isLoading) return <Spinner />;
+
+  function handlUpdate(e, field) {
+    const { value } = e.target;
+
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
+
+  return (
+    <Form>
+      <FormRow label="Minimum nights/booking">
+        <Input
+          type="number"
+          id="min-nights"
+          disabled={isUpdating}
+          defaultValue={minBookingLength}
+          onBlur={(e) => handlUpdate(e, "minBookingLength")}
+        />
+      </FormRow>
+      <FormRow label="Maximum nights/booking">
+        <Input
+          type="number"
+          id="max-nights"
+          disabled={isUpdating}
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handlUpdate(e, "maxBookingLength")}
+        />
+      </FormRow>
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          id="max-guests"
+          disabled={isUpdating}
+          defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handlUpdate(e, "maxGuestsPerBooking")}
+        />
+      </FormRow>
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          id="breakfast-price"
+          disabled={isUpdating}
+          defaultValue={breakfastPrice}
+          onBlur={(e) => handlUpdate(e, "breakfastPrice")}
+        />
+      </FormRow>
+    </Form>
+  );
+}
+
+export default UpdateSettingsForm;
